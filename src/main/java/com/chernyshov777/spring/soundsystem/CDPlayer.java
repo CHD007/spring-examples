9904c03da83940dc303aa73966b81b1e4af3f4a0
chd007 <chernyshov777@gmail.com>
2018-02-25 20:54:58 +0300

    bean configuration by java-config

--- src/main/java/com/chernyshov777/spring/soundsystem/CDPlayer.java
+++ src/main/java/com/chernyshov777/spring/soundsystem/CDPlayer.java
@@ -1,14 +1,9 @@
 package com.chernyshov777.spring.soundsystem;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class CDPlayer implements MediaPlayer {
 
     private CompactDisc cd;
 
-    @Autowired
     public CDPlayer(CompactDisc cd) {
         this.cd = cd;
     }
--- src/main/java/com/chernyshov777/spring/soundsystem/CDPlayerConfig.java
+++ src/main/java/com/chernyshov777/spring/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,17 @@
 package com.chernyshov777.spring.soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class CDPlayerConfig {
+    @Bean
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CDPlayer cdPlayer() {
+        return new CDPlayer(sgtPeppers());
+    }
 }
