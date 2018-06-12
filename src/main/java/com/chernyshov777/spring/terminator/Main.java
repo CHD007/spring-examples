6e88873883a353e20ecf8d183fbebcad48d34944
chd007 <chernyshov777@gmail.com>
2018-06-12 16:58:57 +0300

    constructor with 3 phases

--- src/main/java/com/chernyshov777/spring/terminator/Main.java
+++ src/main/java/com/chernyshov777/spring/terminator/Main.java
@@ -5,10 +5,5 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PropertiesConfiguration.class, QuotersConfig.class);
-        Quoter bean = context.getBean(Quoter.class);
-        while (true) {
-            Thread.sleep(100);
-            bean.sayQuote();
-        }
     }
 }
---
+++ src/main/java/com/chernyshov777/spring/terminator/PostProxyInvokerContextListener.java
@@ -0,0 +1,41 @@
+package com.chernyshov777.spring.terminator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Component
+public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
+    private ConfigurableListableBeanFactory factory;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        ApplicationContext applicationContext = event.getApplicationContext();
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String name : beanDefinitionNames) {
+            // there is some beans can be proxies so we need get bean definition instead of bean class
+            BeanDefinition beanDefinition = factory.getBeanDefinition(name);
+            String beanClassName = beanDefinition.getBeanClassName();
+            try {
+                Class<?> originalClass = Class.forName(beanClassName);
+                Method[] methods = originalClass.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(PostProxy.class)) {
+                        Object bean = applicationContext.getBean(name);
+                        Method proxyMethod = bean.getClass().getMethod(method.getName(), method.getParameterTypes());
+                        proxyMethod.invoke(bean);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
